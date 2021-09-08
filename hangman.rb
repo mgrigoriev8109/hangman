c8fc90beed58b471b4ac36a50bca37119f1d3698
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-08 12:02:23 -0400

    Pseudocode and dictionary of words

---
+++ hangman.rb
@@ -0,0 +1,12 @@
+#when game starts script loads dictionary
+  #randomly selects word between 5 and 12 chars for secret word
+
+#every turn player makes a guess
+  #if out of guesses player loses
+  
+#display guesses player has left before game ends
+  #iterative instance variable that decreases every time method run
+
+#display correct (&incorrect)letters that have been chosen
+  #two different arrays
+  #when guesses_array.include?(secret_word_array) player wins
